5a727942d696d48f3b2edb27dba66e9dd3adaab0
Hieu Ho <55455388+qqq12598@users.noreply.github.com>
2021-06-10 11:46:34 +0700

    feature: add model to scene

--- app/src/main/java/hcmus/hieuho/arcore/MainActivity.java
+++ app/src/main/java/hcmus/hieuho/arcore/MainActivity.java
@@ -1,53 +1,105 @@
 package hcmus.hieuho.arcore;
 
+import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
-
+import com.google.ar.core.Anchor;
 import com.google.ar.core.ArCoreApk;
+import com.google.ar.core.Session;
+import com.google.ar.core.exceptions.UnavailableUserDeclinedInstallationException;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
 import hcmus.hieuho.arcore.helpers.CameraPermissionHelper;
 
 public class MainActivity extends AppCompatActivity {
-
+    // requestInstall(Activity, true) will triggers installation of
+    // Google Play Services for AR if necessary.
+    private boolean mUserRequestedInstall = true;
+    private Session mSession=null;
+    private ArFragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        maybeEnableArButton();
-
         setContentView(R.layout.activity_main);
+        fragment=(ArFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+        fragment.setOnTapArPlaneListener((hitResult, plane, motionEvent) -> {
+            Anchor anchor = hitResult.createAnchor();
+                    ModelRenderable.builder()
+                .setSource(this, R.raw.cheetah)
+                .build()
+                .thenAccept(renderable -> addModelToScene(anchor,renderable))
+                .exceptionally(throwable -> {
+                    Toast.makeText(MainActivity.this,"Model can't be Loaded", Toast.LENGTH_SHORT).show();
+                    return null;
+                });
+        });
+    }
 
+    private void addModelToScene(Anchor anchor,ModelRenderable modelRenderable){
+        AnchorNode anchorNode=new AnchorNode(anchor);
+        TransformableNode transformableNode=new TransformableNode(fragment.getTransformationSystem());
+        transformableNode.setParent(anchorNode);
+        transformableNode.setRenderable(modelRenderable);
+        fragment.getArSceneView().getScene().addChild(anchorNode);
+        transformableNode.select();
     }
 
     @Override
-    protected void onPause() {
-        super.onPause();
+    protected void onResume() {
+        super.onResume();
         if (!CameraPermissionHelper.hasCameraPermission(this)) {
             CameraPermissionHelper.requestCameraPermission(this);
             return;
         }
 
+        // Ensure that Google Play Services for AR and ARCore device profile data are
+        // installed and up to date.
+        try {
+            if (mSession == null) {
+                switch (ArCoreApk.getInstance().requestInstall(this, mUserRequestedInstall)) {
+                    case INSTALLED:
+                        // Success: Safe to create the AR session.
+                        mSession = new Session(this);
+                        break;
+                    case INSTALL_REQUESTED:
+                        // When this method returns `INSTALL_REQUESTED`:
+                        // 1. ARCore pauses this activity.
+                        // 2. ARCore prompts the user to install or update Google Play
+                        //    Services for AR (market://details?id=com.google.ar.core).
+                        // 3. ARCore downloads the latest device profile data.
+                        // 4. ARCore resumes this activity. The next invocation of
+                        //    requestInstall() will either return `INSTALLED` or throw an
+                        //    exception if the installation or update did not succeed.
+                        mUserRequestedInstall = false;
+                        return;
+                }
+            }
+        } catch (UnavailableUserDeclinedInstallationException e) {
+            // Display an appropriate message to the user and return gracefully.
+            Toast.makeText(this, "TODO: handle exception " + e, Toast.LENGTH_LONG)
+                    .show();
+            return;
+        } catch (Exception exception) {
+            return;  // mSession remains null, since session creation has failed.
+        }
     }
 
-    void maybeEnableArButton() {
-        ArCoreApk.Availability availability = ArCoreApk.getInstance().checkAvailability(this);
-        if (availability.isTransient()) {
-            // Continue to query availability at 5Hz while compatibility is checked in the background.
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    maybeEnableArButton();
-                }
-            }, 200);
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        super.onRequestPermissionsResult(requestCode, permissions, results);
+        if (!CameraPermissionHelper.hasCameraPermission(this)) {
+            Toast.makeText(this, "Camera permission is needed to run this application", Toast.LENGTH_LONG)
+                    .show();
+            if (!CameraPermissionHelper.shouldShowRequestPermissionRationale(this)) {
+                // Permission denied with checking "Do not ask again".
+                CameraPermissionHelper.launchPermissionSettings(this);
+            }
+            finish();
         }
-//        if (availability.isSupported()) {
-//            mArButton.setVisibility(View.VISIBLE);
-//            mArButton.setEnabled(true);
-//        } else { // The device is unsupported or unknown.
-//            mArButton.setVisibility(View.INVISIBLE);
-//            mArButton.setEnabled(false);
-//        }
     }
 }
